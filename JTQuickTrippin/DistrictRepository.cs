5318c7c74ac7521c5c87ad1d7c726f6b499ab8b9
Kyle <kylebennett9514@gmail.com>
2023-02-06 21:14:34 -0600

    Added classes

--- JTQuickTrippin/DistrictRepository.cs
+++ JTQuickTrippin/DistrictRepository.cs
@@ -3,10 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Formats.Asn1.AsnWriter;
 
 namespace JTQuickTrippin
 {
-    internal class DistrictRepository
+    class DistrictRepository
     {
+        // we're using a static list here instead of a proper database
+        // but the general idea still holds up once we cover databases
+        // it's just another piece of code to replace and refactor nbd
+        static List<District> _districts = new List<District>();
+
+        public List<District> GetDistricts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveNewDistrict(District district)
+        {
+            throw new NotImplementedException();
+        }
     }
+    class StoreRepository
+    {
+        static List<Store> _stores = new List<Store>();
+
+        public List<Store> GetStores();
+
+    {
+        throw new NotImplementedException();
+    }
+
+}
 }
--- JTQuickTrippin/Program.cs
+++ JTQuickTrippin/Program.cs
@@ -4,13 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(@"QuikTrip Management Systems
+Console.WriteLine(@"QuikTrip Management Systems
                                 
 1. Enter District Sales
 2. Generate District Report
 3. Add New Employee
 4. Add a Store/District
 5. Exit");
+
+
         }
     }
 }
