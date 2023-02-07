44e0e9fd6b79cb2caacabc3ad172bd1b3400ebda
Kyle Bennett <kylebennett95@charter.net>
2023-02-07 16:16:10 -0600

    Added logic for the beginning of the app, and store class

--- JTQuickTrippin/Program.cs
+++ JTQuickTrippin/Program.cs
@@ -4,13 +4,35 @@
     {
         static void Main(string[] args)
         {
-Console.WriteLine(@"QuikTrip Management Systems
-                                
-1. Enter District Sales
-2. Generate District Report
-3. Add New Employee
-4. Add a Store/District
-5. Exit");
+            void printOptions()
+            {
+                Console.WriteLine("QuikTrip Management Systems");
+                Console.WriteLine("1. Enter District Sales");
+                Console.WriteLine("2. Generate District Report");
+                Console.WriteLine("3. Add New Employee");
+                Console.WriteLine("4. Add a Store/District");
+                Console.WriteLine("5. Exit");
+            }
+            int option = 0;
+            do
+            {
+                printOptions();
+                try
+                {
+                    option = int.Parse(Console.ReadLine());
+                }
+                catch { }
+                if (option == 1) { Console.WriteLine("Enter district sales"); }
+                else if(option == 2) { Console.WriteLine("Generating report"); }
+                else if (option == 3) { Console.WriteLine("Add new employee"); }
+                else if (option == 4) { Console.WriteLine("Add store/district"); }
+                else if (option == 5) { break; }
+                else { option = 0; }
+            }
+            while (option!= 5);
+            Console.WriteLine("Thank you, have a nice day!");
+
+
 
 
         }
