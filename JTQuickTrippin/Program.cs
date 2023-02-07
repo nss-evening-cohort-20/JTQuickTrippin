589b9585fe1f5e63215e0e33b8e988711338c5d9
alexcueto88 <wonderwomanoftech@gmail.com>
2023-02-06 18:54:59 -0600

    Add project files.

---
+++ JTQuickTrippin/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace JTQuickTrippin
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
